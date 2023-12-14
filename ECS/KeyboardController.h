bf4c7749f62915e5fef6a9634d6ec8c2e32b12d4
Amine-ZGHAL <zghalamine04@gmail.com>
2023-12-14 11:32:58 +0100

    Develop projectile directions

--- ECS/KeyboardController.h
+++ ECS/KeyboardController.h
@@ -27,22 +27,44 @@ public:
             case SDLK_UP:
                 transform->velocity.y = -1;
                 sprite->Play("Walk");
+                //update direction
+                transform->x_direction = 0;
+                transform->y_direction = -1;
                 break;
             case SDLK_DOWN:
                 transform->velocity.y = 1;
                 sprite->Play("Walk");
+                //update direction
+                transform->x_direction = 0;
+                transform->y_direction = 1;
                 break;
             case SDLK_LEFT:
                 transform->velocity.x = -1;
                 sprite->Play("Walk");
                 sprite->spriteFlip = SDL_FLIP_HORIZONTAL; //flips on the x axis
+                //update direction
+                transform->x_direction = -1;
+                transform->y_direction = 0;
                 break;
             case SDLK_RIGHT:
                 transform->velocity.x = 1;
                 sprite->Play("Walk");
+                //update direction
+                transform->x_direction = 1;
+                transform->y_direction = 0;
                 break;
             case SDLK_SPACE:
-                Game::assets->CreateProjectile(Vector2D(transform->position.x+(transform->width*transform->scale),transform->position.y),Vector2D(1,0),200,2,"projectile");
+                if (transform->x_direction == 0)
+                {
+                    if (transform->y_direction == -1)
+                    {
+                        Game::assets->CreateProjectile(Vector2D(transform->position.x+(transform->width*transform->scale)*0.5,transform->position.y-50),Vector2D(transform->x_direction,transform->y_direction),200,3,"projectile");
+                    }
+                }
+                else
+                {
+                    Game::assets->CreateProjectile(Vector2D(transform->position.x+(transform->width*transform->scale)+20,transform->position.y-20),Vector2D(transform->x_direction,transform->y_direction),200,3,"projectile");
+                }
                 break;
             default:
                 break;
