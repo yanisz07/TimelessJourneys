e6d80d0d6d1be00c34a03b77551be818e6b623e1
Joaquin-Sanchez-Deramond <jisanchez3@uc.cl>
2023-12-17 21:32:13 +0100

    Last changes to weapon Component
    Prepare to merge with melee.

--- ECS/weaponComponent.h
+++ ECS/weaponComponent.h
@@ -8,19 +8,31 @@
 class WeaponComponent : public Component
 {
 public:
-    WeaponComponent()
-    {
-        damage = 0;
-    }
+    WeaponComponent(){};
 
     ~WeaponComponent() override = default;
+
     int attack(SDL_Event event)
     {
+        if(event.type == SDL_KEYDOWN)
+        {
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_z:
+                std::cout << "Hello" << std::endl;
+            }
+        }
+        return 0;
+    }
+
+    int closeAttack()
+    {
+
         return 0;
     }
 private:
-    std::string name;
-    int damage;
+    std::string name = "Standard weapon ";
+    int damage = 0;
 };
 
 #endif // WEAPONCOMPONENT_H
