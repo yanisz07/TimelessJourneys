248ef20e00b9302c0d0584a7fd3593d513e03d63
gabin-lassus <gabin.lassus@gmail.com>
2023-12-30 16:40:58 -0500

    Update setting.h
    add variable keeping track of full screen

--- setting.h
+++ setting.h
@@ -8,7 +8,8 @@ extern std::filesystem::path projectDir;
 class Setting {
 public:
     static void toggleSettingState(bool &isSettingsOpen);
-    static void renderSetting(SDL_Renderer* renderer, bool isSettingsOpen,const SDL_Point& mousePosition);
+    static void renderSetting(SDL_Renderer* renderer, bool isSettingsOpen, const SDL_Point& mousePosition, bool isFullscreen);
+
 };
 
 
