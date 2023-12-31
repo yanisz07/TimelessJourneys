58cfd9b79f3a8ad1c072cb3a2eb8bb2edc713259
gabin-lassus <gabin.lassus@gmail.com>
2023-12-30 19:01:21 -0500

    Update setting.h

--- setting.h
+++ setting.h
@@ -8,7 +8,7 @@ extern std::filesystem::path projectDir;
 class Setting {
 public:
     static void toggleSettingState(bool &isSettingsOpen);
-    static void renderSetting(SDL_Renderer* renderer, bool isSettingsOpen, const SDL_Point& mousePosition, bool isFullscreen);
+    static void renderSetting(SDL_Renderer* renderer, bool isSettingsOpen, const SDL_Point& mousePosition,bool isFullscreen , bool isMusic);
 
 };
 
