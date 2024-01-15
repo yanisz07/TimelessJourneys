db270aec121c656e725bfadf98bb298c1eb25549
Maurice Constantin Matar <mcmmatar@gmail.com>
2024-01-15 14:19:12 +0100

    Improved sword constructor
    I separated the declaration and the definition added some things to the constructor.l

--- sword.cpp
+++ sword.cpp
@@ -8,23 +8,25 @@ using namespace std;
 
 std::filesystem::path ProjectDir = std::filesystem::current_path();
 
-// Function to create a Sword item
-/* Sword createSword(SDL_Renderer* renderer) {
-    std::string swordName = "Excalibur";
+// Function to create a Melee Sword item
+Melee::Melee(SDL_Texture* icon, int damage, int durability)
+    : InventoryItem(icon, items::Sword){
+    this->damage = damage;
+    this->durability = durability;
+
     std::string swordIconPath = (ProjectDir / ".." / "TimelessJourneys" / "assets" / "sword.png").string();
     SDL_Texture* swordIcon = TextureManager::LoadTexture(swordIconPath.c_str());
 
-    if (swordIcon) {
+    /* if (swordIcon) {
         int swordDamage = 20; // Adjust as needed
         int swordDurability = 100; // Adjust as needed
 
-        Sword swordItem(swordIcon, swordName, swordDamage, swordDurability);
-        return swordItem;
+        //Melee swordItem(swordIcon, swordName, swordDamage, swordDurability);
+        //return swordItem;
     } else {
         cerr << "Failed to load texture for sword: " << swordName << endl;
         // Return a default Sword with null texture in case of failure
-        return Sword(nullptr, swordName, 0, 0);
-    }
+        return Melee(nullptr, swordName, 0, 0);
+    } */
 }
 
-*/
